adecb1336c1f10ac2e4828c0e41e5b549352a3a1
yurirnascimento <yuri.rsl@gmail.com>
2021-11-17 14:46:32 -0300

    Create create-shipment.php

---
+++ PHP/curl/create-shipment.php
@@ -0,0 +1,56 @@
+<?php
+
+$url = "https://sandbox.tristarexpress.com/v1/create-account";
+
+$NAME = "Your Name";
+$EMAIL = "your.email@tristarexpress.com";
+$PASSWORD = "password";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+	'Accept: application/json',
+	'Content-Type: application/x-www-form-urlencoded;charset=UTF-8'
+]);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS,
+	http_build_query(["name" => $NAME, "email" => $EMAIL, "password" => $PASSWORD])
+);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+if(!($httpCode >= 200 && $httpCode < 300)) {
+	// Exception
+	http_response_code(500);
+	echo $response;
+	exit;
+}
+	
+try {
+	
+	$jsonResponse = json_decode($response, false, 512, JSON_THROW_ON_ERROR);
+	
+} catch (JsonException $e) {
+	
+	http_response_code(500);
+	echo $response;
+	exit;
+	
+}
+	
+// TODO: save the bearer_token
+	
+var_dump($jsonResponse);
+
+/* EXPECT:
+
+object(stdClass)#1 (2) {
+	["id"]=> int(0)
+	["bearer_token"]=>
+	string(42) "0|fhdwUpwW56BEqDVFV9LQKARhVvSrJobQCNzWBpZb"
+}
+
+*/
